608cc2d816e30d4583ac3b6229690ac6bfc0d7c5
oussama elbouqi <Elbouqi.oussama@gmail.com>
2026-06-27 12:37:55 +0100

    Remove basename from HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import AuthProvider from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename='/ecommerce-react-bootstrap'>
+    <HashRouter >
       <AuthProvider>
         <CartProvider>
           <App />
